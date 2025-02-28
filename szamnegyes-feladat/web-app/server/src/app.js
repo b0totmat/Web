4da9004b4b7183cd3e22080cab86570390dd68b8
Toth-Mate <t0thm4t3@gmail.com>
2025-02-28 13:02:52 +0100

    Új számnégyes felvétele (POST metódus)

--- szamnegyes-feladat/web-app/server/src/app.js
+++ szamnegyes-feladat/web-app/server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import fs from 'fs'
 
 const app = express()
 
@@ -8,7 +9,28 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    fs.rea
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    fs.readFile('./src/fours.json', (err, data) => {
+        if(err) {
+            res.status(400)
+        } else {
+            const fours = JSON.parse(data)
+            const newFour = req.body
+
+            fours.push(newFour)
+
+            fs.writeFile('./src/fours.json', JSON.stringify(fours), (error) => {
+                if(error) {
+                    res.status(400)
+                }
+                res.status(200)
+            })
+        }
+    })
 })
 
 app.listen(3000)
