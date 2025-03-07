7c19de3db0968ab669ffcc93f47a1f26c0df2c65
Toth-Mate <t0thm4t3@gmail.com>
2025-03-07 11:58:41 +0100

    Menü és alap elrendezés

--- szamnegyes-feladat/web-app/client/src/App.vue
+++ szamnegyes-feladat/web-app/client/src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <header>
+        <Navbar />
+    </header>
+    <main class="mt-3">
+      <RouterView />
+    </main>
+  </div>
 </template>
